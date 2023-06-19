6931e614b8feffb15169bbe7eb0820bbf76eee42
Anton Demyanchuk <anton.demyanchuk@gmail.com>
2023-06-20 00:41:13 +0300

    Id fixed

--- SoundsGoodCRM/DAO/EntityWithId.cs
+++ SoundsGoodCRM/DAO/EntityWithId.cs
@@ -1,7 +1,11 @@
-﻿namespace SoundsGoodCRM.DAO
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace SoundsGoodCRM.DAO
 {
     public class EntityWithId
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int Id { get; set; }
     }
 }
